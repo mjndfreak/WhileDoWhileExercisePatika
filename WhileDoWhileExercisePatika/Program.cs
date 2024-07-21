8172cc3400485c31d17c373f0aa28fac77d8e543
mjndfreakk <cagdasergencc@gmail.com>
2024-07-21 19:31:06 +0300

    Add project files.

---
+++ WhileDoWhileExercisePatika/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+using System.Threading.Channels;
+
+int counter = 0;
+Console.Write("Limit giriniz: ");
+int limit = int.Parse(Console.ReadLine());
+if (limit >= 0)
+{
+    while (counter <= limit)
+    {
+        Console.Write(counter + ": ");
+        Console.WriteLine(" Ben bir Patika'lıyım ");
+        counter++;
+        
+    }
+    
+}
+else
+{
+    while (counter >= limit)
+    {
+        Console.Write(Math.Abs(counter) + ": ");
+        Console.WriteLine(" Ben bir Patika'lıyım ");
+        counter--;
+    }
+}
+
+
+
+
+int counter2 = 0;
+Console.Write("Limit giriniz: ");
+var limit2 = Convert.ToInt16(Console.ReadLine());
+do
+{
+    Console.Write(counter2 + ": ");
+    Console.WriteLine(" Ben bir Patika'lıyım ");
+    counter2++;
+    
+}
+while (counter2 <= Math.Abs(limit2));
+
+
+
